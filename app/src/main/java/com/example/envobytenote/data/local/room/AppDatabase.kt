59c09f09c532db9836209bdd270bc69e7ed92d91
Shamsul1 <Banglish123@gmail.com>
2025-10-16 01:37:36 +0600

    import optimized

--- app/src/main/java/com/example/envobytenote/data/local/room/AppDatabase.kt
+++ app/src/main/java/com/example/envobytenote/data/local/room/AppDatabase.kt
@@ -1,16 +1,8 @@
 package com.example.envobytenote.data.local.room
 
-import android.content.Context
-
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
-import javax.inject.Singleton
 
 
 @Database(entities = [Note::class], version = 1, exportSchema = false)
